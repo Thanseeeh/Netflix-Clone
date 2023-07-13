6315b92df8c407671544253e3524a86478c1ef0b
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-07-13 11:59:33 +0530

    netflix clone completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import NavBar from './Components/Navbar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
+import { Originals, Action, Trending, ComedyMovies } from './urls';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost urls={Originals} title="Netflix Originals" />
+      <RowPost urls={Action} title="Action" isSmall />
+      <RowPost urls={Trending} title="Trending" isSmall />
+      <RowPost urls={ComedyMovies} title="ComedyMovies" isSmall />
     </div>
   );
 }
---
+++ src/urls.js
@@ -0,0 +1,6 @@
+import { API_KEY } from "./Constents/Constents"
+
+export const Originals = `discover/tv?api_key=${API_KEY}&with_networks=213`
+export const Action = `discover/movie?api_key=${API_KEY}&with_genres=28`
+export const Trending = `trending/all/week?api_key=${API_KEY}&language=en-US`
+export const ComedyMovies = `discover/movie?api_key=${API_KEY}&with_genres=35`
